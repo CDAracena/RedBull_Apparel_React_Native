482b095db0662dd4e274d2bd5fd084b056375318
CDAracena <cdaracena@gmail.com>
2019-05-02 21:46:22 -0400

    redux integration into cart component, flatlist render

--- android/app/src/components/Cart.js
+++ android/app/src/components/Cart.js
@@ -1,14 +1,30 @@
 import React, {useState, useEffect} from 'react';
 import {Platform, StyleSheet, Text, View, TextInput, FlatList, Image, ScrollView, Button} from 'react-native';
+import {connect} from 'react-redux';
 
 
 const Cart = (props) => {
-    
+    const {Cart} = props.shoppingCart
     return (
         <View>
-            <Text>This is the cart</Text>
+           <FlatList
+           data={Cart}
+           keyExtractor={(item) => item.id.toString()}
+           renderItem={({item})=> <View><Image source={{uri: item.images[0].src}} style={{width: 66, height: 54}}/><Text>{item.title}</Text></View>}/>
         </View>
     )
 }
 
-export default Cart;
+const mapStateToProps = ({shoppingCart})=> {
+    return {
+        shoppingCart
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps) (Cart);
