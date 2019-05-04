fd8be4ffa4619894b6c3f974ae47e7a42011b09c
CDAracena <cdaracena@gmail.com>
2019-05-04 02:45:19 -0400

    importing touchable opacity

--- android/app/src/components/Cart.js
+++ android/app/src/components/Cart.js
@@ -1,8 +1,7 @@
 import React, {useState, useEffect} from 'react';
-import {Platform, StyleSheet, Text, View, TextInput, FlatList, Image, ScrollView, Button} from 'react-native';
+import {Platform, StyleSheet, Text, View, FlatList, Image, TouchableOpacity} from 'react-native';
 import {connect} from 'react-redux';
 import {Icon} from 'react-native-elements';
-import { TouchableOpacity } from 'react-native-gesture-handler';
 
 
 const Cart = (props) => {
@@ -10,10 +9,18 @@ const Cart = (props) => {
     return (
         <View style={styles.mainContainer}>
            <FlatList
-        
            data={Cart}
            keyExtractor={(item) => item.id.toString()}
-           renderItem={({item})=> <View style={styles.listItemContainer}><Image source={{ uri: item.images[0].src}} style={{width: 66, height: 54}}/><Text style={styles.itemTitle}>{item.title}</Text><Text>${item.price}</Text><View style={styles.counterContainer}><Icon name="plus-square" type="font-awesome" color="#880D1E" component={TouchableOpacity}/><Text>{item.itemCount}</Text><Icon name="minus-square" type="font-awesome" color="#880D1E" component={TouchableOpacity}/></View></View>}/>
+           renderItem={({item})=> <View style={styles.listItemContainer}>
+           <Image source={{ uri: item.images[0].src}} style={{width: 66, height: 54}}/>
+           <Text style={styles.itemTitle}>{item.title}</Text>
+           <Text>${item.price}</Text>
+           <View style={styles.counterContainer}>
+           <Icon name="plus-square" type="font-awesome" color="#880D1E" component={TouchableOpacity}/>
+           <Text>{item.itemCount}</Text>
+           <Icon name="minus-square" type="font-awesome" color="#880D1E" component={TouchableOpacity}/>
+           </View>
+           </View>}/>
         </View>
     )
 }
